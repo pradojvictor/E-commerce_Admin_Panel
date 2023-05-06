5712007c1815b21ca6a1785ee7396ab18b6a0aed
pradojvictor <joaovictorred@hotmail.com>
2023-05-06 17:52:17 -0300

    add is model to database to products

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+const { Schema, model, models } = require("mongoose");
+
+const ProductSchema = new Schema({
+    title: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+});
+
+export const Product = models.Product || model('Product', ProductSchema);
