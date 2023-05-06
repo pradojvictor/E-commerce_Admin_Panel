7c809a4c24ab48fa100e20fd4f05b2f13f3eec64
pradojvictor <joaovictorred@hotmail.com>
2023-05-06 17:47:39 -0300

    add component Layout

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "./Nav";
+
+
+
+export default function Layout({ children }) {
+
+    const { data: session } = useSession();
+    if (!session) {
+        return (
+            <div className="bg-blue-900 w-screen h-screen flex items-center">
+                <div className="text-center w-full">
+                    <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="bg-blue-900 min-h-screen flex">
+            <Nav />
+            <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+                {children}
+            </div>
+        </div>
+    );
+}
